53b316ea916ff7e8708f29d8ae04a8607c1c870a
boahs <39522778+boahs@users.noreply.github.com>
2020-04-04 18:08:07 -0400

    Create Calculaet BMI.js

---
+++ 8kyu/Calculaet BMI.js
@@ -0,0 +1,17 @@
+function bmi(weight, height) {
+  
+    var bmi     = weight / Math.pow(height, 2),
+        message = 'Obese';
+    
+    if (bmi <= 18.5) {
+      message = 'Underweight';
+    } else if (bmi <= 25.0) {
+      message = 'Normal';
+    } else if (bmi <= 30.0) {
+      message = 'Overweight';
+    }
+    
+    return message;
+  }
+  
+  bmi(80, 1.80)

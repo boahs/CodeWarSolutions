e6887a37d58b68520d2e52a6066a782d810086aa
boahs <39522778+boahs@users.noreply.github.com>
2020-05-20 10:51:08 -0400

    Create Grasshopper - Create the rooms.js

---
+++ 8kyu/Grasshopper - Create the rooms.js
@@ -0,0 +1,17 @@
+var rooms = {
+    spooky: {
+    name: 'Spooky room',
+    description: 'Very spooky...oooooooOOooOO',
+    completed: 'You rock'
+    },
+    jungle: {
+    name: 'jungle',
+    description: 'Viney',
+    completed: 'Good job!'
+    },
+    Volcano: {
+    name: 'Volacano',
+    description: ' Very HOT',
+    completed: 'Fantastic job, adventurer!'
+    }
+  }

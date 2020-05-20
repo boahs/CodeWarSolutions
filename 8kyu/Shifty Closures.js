b9547571810ed41328d58ce17a54de7c885e75f8
boahs <39522778+boahs@users.noreply.github.com>
2020-05-20 14:38:52 -0400

    Create Shifty Closures.js

---
+++ 8kyu/Shifty Closures.js
@@ -0,0 +1,9 @@
+
+var greet_abe = function() {
+    var name = 'Abe';
+    return "Hello, " + name + '!';
+  };
+  var greet_ben = function() {
+    name = 'Ben';
+    return "Hello, " + name + '!';
+  };

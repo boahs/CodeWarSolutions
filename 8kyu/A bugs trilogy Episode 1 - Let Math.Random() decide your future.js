d94cba98b2b0158d514b69a94ae696ddc797fb76
boahs <39522778+boahs@users.noreply.github.com>
2020-05-24 22:29:56 -0400

    Create A bugs trilogy Episode 1 - Let Math.Random() decide your future.js

---
+++ 8kyu/A bugs trilogy Episode 1 - Let Math.Random() decide your future.js
@@ -0,0 +1,15 @@
+function yourFutureCareer() {
+
+    let career = Math.random() 
+		if (career <= 0.32) {
+		  return 'FrontEnd Developer'
+        }else if(career >= 0.65){ 
+			return 'BackEnd Developer'
+		} 
+			return 'Full-Stack Developer'
+        
+}
+
+
+
+yourFutureCareer();

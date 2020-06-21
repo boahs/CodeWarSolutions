393040fd9628bf82ab123f6a1b712dd08d81aac7
boahs <39522778+boahs@users.noreply.github.com>
2020-06-21 08:05:25 -0400

    Create Leonardo Dicaprio and Oscars.js

---
+++ 8kyu/Leonardo Dicaprio and Oscars.js
@@ -0,0 +1,15 @@
+function leo(oscar){
+    if (oscar !==86 && oscar < 88){
+    return 'When will you give Leo an Oscar?';
+    }else if(oscar > 88){
+    return 'Leo got one already!';
+    }
+    switch(oscar){
+    case 88:
+    return 'Leo finally won the oscar! Leo is happy'
+    break;
+    case 86:
+    return 'Not even for Wolf of wallstreet?!';
+    break;
+    }
+    }
